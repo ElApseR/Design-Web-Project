e16e47177c40cf63b4a7ae69bdda1b58986109e0
ElApseR <HyunhoCho@johyeonhoui-MacBook-Pro.local>
2018-01-29 20:36:42 +0900

    Flask For Web Practice
    Flask를 이용해서 local에서 test해보

---
+++ web_test/web_flask.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import pandas as pd
+from bokeh.charts import Bar, output_file, show
+from bokeh.embed import components
+
+app = Flask(__name__)
+
+df = pd.read_csv('data.csv', encoding='utf-8')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/select')
+def select():
+    return render_template('select.html')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
