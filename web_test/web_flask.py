5de290f3686fb34f75fa9617d952dafd7e47eb4a
cen0215 <pcn03119@gmail.com>
2018-01-29 20:59:21 +0900

    app.run modified
    host='0.0.0.0'

--- web_test/web_flask.py
+++ web_test/web_flask.py
@@ -17,4 +17,4 @@ def select():
     return render_template('select.html', table=table)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True,host='0.0.0.0')
